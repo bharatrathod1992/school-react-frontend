86fc6a3c10365f08a90db9726017b7c4271dd10f
RakeshArya07 <rakesharya.nitdgp@gmail.com>
2017-02-06 11:48:27 +0530

    added test cases and css

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Sum from './components/Sum/Sum';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Class Front</h1>
+        <Sum />
+      </div>
+    );
+  }
+}
+
+export default App;
